6dc65d31ce25a0215cf56902fbd5dc1b6cf5e1a4
Jmcantone <jmcantone@gmail.com>
2018-10-19 12:42:56 -0300

    commit inicial

---
+++ Multiplicar/multiplicar.js
@@ -0,0 +1,43 @@
+//requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido ${base} no es un número`);
+            return;
+        }
+
+        let data = ''
+
+        for (let index = 1; index <= limite; index++) {
+            data += `${base} * ${index} = ${base * index}\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err)
+            else
+                resolve(`tabla-${base}.txt`)
+        });
+
+    });
+
+};
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log('======================'.green);
+    console.log(`== Tabla de ${base} ==`.green);
+    console.log('======================'.green);
+
+    for (let index = 1; index <= limite; index++) {
+        console.log(`${base} * ${index} = ${base * index}`);
+    }
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
